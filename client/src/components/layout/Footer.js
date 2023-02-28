ec39cc4e1ed836cb899157593daa7f219d7efd58
Abutalib Khan <thetalibkhan555@gmail.com>
2023-02-28 12:28:55 +0530

    navigation menu and page design

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
-      
-      <h1 className='text-center'>All right reserved &copy; Abutalib Khan</h1>
+    <div className="footer">
+      <h1 className="text-center">All right reserved &copy; Abutalib Khan</h1>
+      <p className="text-center mt-3">
+      <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+      <Link to="/policy">privacy Policy</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { GiShoppingBag } from "react-icons/gi";
 
 const Header = () => {
   return (
@@ -18,27 +19,17 @@ const Header = () => {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <NavLink to="/" className="navbar-brand" href="#">
-              Hidden brand
-            </NavLink>
+            <Link to="/" className="navbar-brand">
+              <GiShoppingBag /> E-commerce app
+            </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink
-                  to="/"
-                  className="nav-link "
-                  aria-current="page"
-                 
-                >
+                <NavLink to="/" className="nav-link " aria-current="page">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink
-                  to="/category"
-                  className="nav-link"
-                  
-                 
-                >
+                <NavLink to="/category" className="nav-link">
                   Category
                 </NavLink>
               </li>
@@ -53,7 +44,7 @@ const Header = () => {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/login" className="nav-link" href="#">
+                <NavLink to="/cart" className="nav-link" href="#">
                   Cart (0)
                 </NavLink>
               </li>
