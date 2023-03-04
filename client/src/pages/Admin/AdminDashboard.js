031b2fa921e2f054daa79a6e64d2a9ca1d226d55
Abutalib Khan <thetalibkhan555@gmail.com>
2023-03-05 00:25:18 +0530

    admin panel dashboard created category product and users

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import AdminMenu from '../../components/layout/AdminMenu'
 import Layout from '../../components/layout/Layout'
 
 const AdminDashboard = () => {
   return (
     <Layout>
-        <h1>Admin Dashboard</h1>
+      <div className='container-fluid m-3 p-3'>
+        <div className='row'>
+        <div className='col-md-3'>
+          <AdminMenu />
+
+        </div>
+        <div className='col-md-9'> Content </div>
+
+        </div>
+
+      </div>
     </Layout>
   )
 }
