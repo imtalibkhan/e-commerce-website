ec82340cfd14a028defdf3de141d322a95ae00cb
Abutalib Khan <thetalibkhan555@gmail.com>
2023-03-07 17:46:31 +0530

    create category part complete

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,22 +1,88 @@
-import React from "react";
-import Layout from './../../components/layout/Layout';
-import AdminMenu from './../../components/layout/AdminMenu';
+import React, { useEffect, useState } from "react";
+import Layout from "./../../components/layout/Layout";
+import AdminMenu from "./../../components/layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
 
 const CreateCategory = () => {
-  return (
-    <Layout title={'Dashboard -create-category'}>
-      <div className="container-fluid m-3 p-3">
+  const [categories, setCategories] = useState([]);
+  const [name,setName] = useState("")
+  //handdle form
 
-    <div className="row">
-      <div className="col-md-3">
-        <AdminMenu />
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    try {
+      const {data} = await axios.post('/api/v1/category/create-category',{name})
+      if(data?.success) {
+        toast.success(`${name} is created`)
+        getAllCategory();
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong in input form")
+    }
+  }
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
+  return (
+    <Layout title={"Dashboard -create-category"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Manage category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName} />
+
+            </div>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((c) => (
+                    <>
+                      <tr>
+                        <td key={c._id}>{c.name}</td>
+
+                        <td>
+                          <button className="btn btn-primary">Edit</button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-md-9">
-        <h1>create category</h1>
-      </div>
-    </div>
-    </div>
-  </Layout>
+    </Layout>
   );
 };
 
